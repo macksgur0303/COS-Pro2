4a35d12ba4ad29f997c600b780ebdd73c08b1e14
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-03-30 17:37:10 +0900

    Add files via upload

---
+++ 2023.03.30/if , else, else if 모두 사용하기.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    char c1 = 'c';
+
+    if (c1 == 'a')
+        printf("a\n");
+    else if (c1 == 'b')
+        printf("b\n");
+    else if (c1 == 'c')
+        printf("c\n");
+    else if (c1 == 'd')
+        printf("d\n");
+    else
+        printf("x\n");
+    return 0;
+}
