9095afd0c35334fa2e7b71aa47d962aa99d9e52a
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-04-05 08:59:47 +0900

    Add files via upload

---
+++ 2023.04.05/알파벳 바꾸기/알파벳 바꾸기.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(char* s) 
+{
+    char* s_ret = (char*)malloc(sizeof(char) * (strlen(s) + 1));
+    strcpy(s_ret, s);
+
+    for (int i = 0; s_ret[i] != 0; ++i) 
+    {
+        if (s_ret[i] == 'a')
+            s_ret[i] = 'z';
+        else if (s_ret[i] == 'z')
+            s_ret[i] = 'a';
+    }
+    return s_ret;
+}
+
+int main()
+{
+    int s_len = 3;
+    char s[] = "abz";
+    char* sum = (char*)malloc(sizeof(char) * ((s_len) + 1));
+
+    for (int i = 0; i < s_len; i++)
+    {
+        sum[i] = solution(s)[i];
+        printf("%c",sum[i]);
+    }
+
+    free(sum);
+    return 0;
+}
---
+++ 2023.04.05/키가 K보다 큰 사람/키가 K보다 큰 사람.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int height[], int height_len, int k) 
+{
+    int answer = 0;
+    for (int i = 0; i < height_len; ++i)
+        if (height[i] > k)
+            answer++;
+    return answer;
+}
+
+int main()
+{
+    int height[] = { 165, 170, 175, 180, 184 };
+    int height_len = 5;
+    int k = 175;
+
+    int sum = solution(height, height_len, k);
+    printf("175보다 큰 키는 ");
+    for (int i = height_len - sum; i < height_len; i++)
+    {
+        printf("%d ", height[i]);
+    }
+    printf("입니다");
+}
