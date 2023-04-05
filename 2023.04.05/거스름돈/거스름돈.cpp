ff1125753da4624065729c2f535092efd831cbe6
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-04-05 13:15:37 +0900

    Add files via upload

---
+++ 2023.04.05/거스름돈/거스름돈.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int price[], int price_len, int money) {
+    int answer = 0;
+    for (int i = 0; i < price_len; i++)
+    {
+        answer += price[i];
+    }
+
+    answer = money - answer;
+
+    return answer;
+}
+
+int main()
+{
+    int price[] = { 2100, 3200, 2100, 800 };
+    int price_len = 4;
+    int money = 10000;
+
+    int answer = solution(price, price_len, money);
+
+    printf("고객이 지불한 돈이 %d원이므로 %d원을 거슬러 줘야 합니다.", money, answer);
+}
---
+++ 2023.04.05/이름에 j 또는 k가 들어가는 사람/이름에 j 또는 k가 들어가는 사람.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(char name_list[][7], int name_list_len) {
+    int answer = 0;
+    for (int i = 0; i < name_list_len; ++i)
+    {
+        for (int j = 0; name_list[i][j] != 0; ++j)
+        {
+            if (name_list[i][j] == 'j' || name_list[i][j] == 'k')
+            {
+                answer++;
+                break;
+            }
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    char name_list[][7] = { "james", "luke", "oliver", "jack" };
+    int name_list_len = sizeof(name_list) / sizeof(char*);
+    int sum = solution(name_list, name_list_len);
+
+    printf("이름에 j 또는 k가 들어가는 사람은 총 %d명입니다.", sum);
+
+}
