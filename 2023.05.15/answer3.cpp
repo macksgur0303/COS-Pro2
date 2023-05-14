95f26a6e527cd5880495aa207a3d7ae8718648b8
macksgur0303 <121844168+macksgur0303@users.noreply.github.com>
2023-05-15 08:41:37 +0900

    Add files via upload

---
+++ 2023.05.15/answer3.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int solution(int arr[], int arr_len)
+{
+	int answer = 0;
+	for (int i = 0; i < arr_len; i++)
+	{
+		for (int j = 0; j < arr_len; j++)
+		{
+			if (arr[i] / 2 == arr[j])
+			{
+				answer++;
+				break;
+			}
+		}
+	}
+	return answer;
+}
+
+int main()
+{
+	int arr[] = { 4, 8, 3 ,6 ,3 };
+	int arr_len = 5;
+	int ret = solution(arr, arr_len);
+	printf("solution 함수의 반환 값은 %d 입니다", ret);
+}
